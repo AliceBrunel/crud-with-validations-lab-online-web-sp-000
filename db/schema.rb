a9cda41675e5b91e5c16bbd324b080e246ebee20
Alice Brunel <alc.brunel@gmail.com>
2019-10-29 12:19:19 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191029115503) do
+ActiveRecord::Schema.define(version: 20191029121621) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
-    t.integer  "released"
+    t.boolean  "released"
     t.integer  "release_year"
     t.string   "artist_name"
     t.string   "genre"
