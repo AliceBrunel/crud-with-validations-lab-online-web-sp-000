ca6ef4c0517af25600ecc646948544c90db32128
Alice Brunel <alc.brunel@gmail.com>
2019-10-29 12:22:52 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'songs#index'
   get '/songs', to: 'songs#index'
   get '/songs/new', to: 'songs#new', as: 'new_song'
   post '/songs', to: 'songs#create'
